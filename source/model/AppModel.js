97d9d9ecf7c40609ee6fc9b82ae6f2fdacfa278c
bjadel <bjawebos@adelberg-online.de>
2016-02-15 22:14:10 +0100

    change version to 1.1.0-3

--- source/model/AppModel.js
+++ source/model/AppModel.js
@@ -3,17 +3,17 @@
  */
 var AppModel = ({
 	initialize: function()  {
-		this.version = "1.1.0";
+		this.version = "1.1.0-3";
     	this.author = "Björn Adelberg";
-    	
+
     	this.isAndroid = false;
     	this.isWeb = true;
-    	
+
     	this.supportMail = "mailto:bjawebos@adelberg-online.de";
     	this.supportHomepage = "http://dev.adelberg-online.de";
 	    this.supportTwitter = "https://twitter.com/bjawebos";
     	this.supportFacebook = "http://goo.gl/FPv0d";
-    	
+
     	this.existsSmallScreen = false;
 	},
 	setExistsSmallScreen: function() {
