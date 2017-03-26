c1b7f6b3bc88f8b467da2a856105a93448842a81
bjadel <bjawebos@adelberg-online.de>
2017-03-26 21:55:40 +0200

    change version

--- source/android/model/AppModel.js
+++ source/android/model/AppModel.js
@@ -3,17 +3,17 @@
  */
 var AppModel = ({
 	initialize: function()  {
-		this.version = "1.1.0";
+		this.version = "1.1.0-4";
     	this.author = "Björn Adelberg";
-    	
+
     	this.isAndroid = true;
     	this.isWeb = false;
-    	
+
     	this.supportMail = "mailto:bjawebos@adelberg-online.de";
     	this.supportHomepage = "http://dev.adelberg-online.de";
 	    this.supportTwitter = "https://twitter.com/bjawebos";
     	this.supportFacebook = "http://goo.gl/FPv0d";
-    	
+
     	this.existsSmallScreen = false;
 	},
 	setExistsSmallScreen: function() {
--- source/model/AppModel.js
+++ source/model/AppModel.js
@@ -3,7 +3,7 @@
  */
 var AppModel = ({
 	initialize: function()  {
-		this.version = "1.1.0-3";
+		this.version = "1.1.0-4";
     	this.author = "Björn Adelberg";
 
     	this.isAndroid = false;
